05e44a349ca7ebb8a2197be1e50b1de8febfe1ab
vitimerida <81519967+vitimerida@users.noreply.github.com>
2021-09-20 10:09:14 +0200

    Update Document Email Sender.py

--- Document Email Sender.py
+++ Document Email Sender.py
@@ -4,7 +4,7 @@ import os
 
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
-#from email.mime.text import MIMEText
+from email.mime.text import MIMEText
 from email import encoders
 
 email_address = os.environ.get("user1")
@@ -13,13 +13,14 @@ email_pass = os.environ.get("pass1")
 msg = MIMEMultipart()
 msg["From"] = email_address
 msg["To"] = "victormerida55@gmail.com"
-msg["Subject"] = input("Name of the file ->  ")
+
 
 desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
 os.chdir(desktop)
 
 
 file = input("file name +  .docx (word) or .pdf :  ")
+msg["Subject"] = file
 attachment = open(file,"rb")
 part = MIMEBase("application","octect-stream")
 part.set_payload(attachment.read())
@@ -30,7 +31,7 @@ with smtplib.SMTP_SSL("smtp.gmail.com",465) as smtp:
     smtp.login(email_address,email_pass)
     smtp.send_message(msg)
 
-print("The assessment has been sent")
+print("The email has been sent")
 
 
 
